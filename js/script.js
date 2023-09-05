100ab9ecb37a86be8cfc09967ae0ae9a47e8371c
sabihDev <8203sabi.rahman@gmail.com>
2023-09-05 05:49:06 -0700

    carousel designed
    1)  function next() is completed
    2)  function prev() is completed
    3)  image changing animation is designed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("hello");
+import { AddClickListener } from './Reuseables.js';
+
+const nextButton = document.querySelector('#next');
+const prevButton = document.querySelector('#prev');
+
+let images = document.querySelectorAll('.carousel-container');
+let index = 0;
+
+AddClickListener(nextButton,next);
+AddClickListener(prevButton,prev)
+
+function next() {
+    images[index].classList.remove('active');
+    index = (index + 1) % images.length;
+    images[index].classList.add('active');
+}
+
+function prev() {
+    images[index].classList.remove('active');
+    index = (index - 1 + images.length) % images.length;
+    images[index].classList.add('active');
+}
