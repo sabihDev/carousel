e9f798b75d618877bbc3d29f1b814a1179fb36ad
sabihDev <8203sabi.rahman@gmail.com>
2023-09-06 05:40:58 -0700

    javascript of carousel 2 is done
    1)  SetControls() is done with logic
    2) UseControls() is also done with logic

--- js/script.js
+++ js/script.js
@@ -1,33 +1,59 @@
-import { AddClickListener } from './Reuseables.js';
+const carouselContainer = document.querySelector(".carousel-container");
+const carouselControlsContainer = document.querySelector(".carousel-controls"); // Changed let to const
+const carouselControls = ['previous', 'next'];
+const carouselItems = document.querySelectorAll('.carousel-item');
 
-let slideIndex = 0;
+class Carousel {
+    constructor(container, items, controls) {
+        this.galleryContainer = container;
+        this.galleryControls = controls;
+        this.carouselArray = [...items];
+    }
 
-function showSlide(n) {
-  const slides = document.getElementsByClassName("carousel-slide");
-  
-  if (n >= slides.length) {
-    slideIndex = 0;
-  }
-  if (n < 0) {
-    slideIndex = slides.length - 1;
-  }
+    UpdateCarousel() { // Changed method name to use camelCase
+        this.carouselArray.forEach(el => {
+            el.classList.remove("carousel-item-1");
+            el.classList.remove("carousel-item-2");
+            el.classList.remove("carousel-item-3");
+            el.classList.remove("carousel-item-4");
+            el.classList.remove("carousel-item-5");
+        });
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`carousel-item-${i + 1}`);
+        });
+    }
 
-  slides[slideIndex].style.display = "block";
+    setCurrentState(direction) { // Changed method name to use camelCase
+        if (direction.className == 'carousel-controls-previous') {
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else {
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.UpdateCarousel(); // Changed method name to use camelCase
+    }
+
+    SetControls() { // Changed method name to use camelCase
+        this.galleryControls.forEach(control => {
+            const createButton = document.createElement('button'); // Fixed the assignment
+            createButton.className = `carousel-controls-${control}`;
+            carouselControlsContainer.appendChild(createButton); // Used appendChild to add the button
+            createButton.innerText = control; // Set the button text
+        });
+    }
+
+    UseControls() { // Changed method name to use camelCase
+        const triggers = [...carouselControlsContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                // e.style.display = 'block'; // This line is not needed
+                this.setCurrentState(control);
+            });
+        });
+    }
 }
 
-function prevSlide() {
-  showSlide(slideIndex -= 1);
-}
-
-function nextSlide() {
-  showSlide(slideIndex += 1);
-}
-
-showSlide(slideIndex);
-
-document.getElementById("prevBtn").addEventListener("click", prevSlide);
-document.getElementById("nextBtn").addEventListener("click", nextSlide);
+const exampleCarousel = new Carousel(carouselContainer, carouselItems, carouselControls);
+exampleCarousel.SetControls();
+exampleCarousel.UseControls();
